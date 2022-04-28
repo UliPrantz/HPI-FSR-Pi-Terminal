a7baca7d69d745723c8628cce2263bfe10f46025
Uli Prantz <uli.prantz@student.hpi.uni-potsdam.de>
2022-04-28 10:02:14 +0200

    Padded uid to the length of 2 digits.

--- lib/infrastructure/chip_scan/chip_scan_dto.dart
+++ lib/infrastructure/chip_scan/chip_scan_dto.dart
@@ -8,7 +8,7 @@ class ChipScanDto {
   ChipScanData toDomain() {
     final StringBuffer stringBuffer = StringBuffer('');
     for (int x in uidData) {
-      final String tmpString = x.toRadixString(16).padLeft(0, '0');
+      final String tmpString = x.toRadixString(16).padLeft(2, '0');
       stringBuffer.write(tmpString);
     }
     return ChipScanData(uuid: stringBuffer.toString());
